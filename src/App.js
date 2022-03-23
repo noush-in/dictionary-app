929bdd36944fb95d5540a8d9587d858ef3c05620
noush-in <noushinheidari1990@gmail.com>
2022-03-23 12:21:08 +0330

    Dictionary component added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import Dictionary from "./Dictionary";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      hello
-      </header>
-    </div>
-  );
+		<div className="container">
+			<div className="App">
+				<header className="App-header text-center">hello</header>
+				<main >
+					<Dictionary />
+				</main>
+				<footer className="App-footer">Coded by Noushin Heidari</footer>
+			</div>
+		</div>
+	);
 }
 
 export default App;
