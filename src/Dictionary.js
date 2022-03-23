58c4039762546d3dcd62f2351b04e97931626141
noush-in <noushinheidari1990@gmail.com>
2022-03-23 20:09:37 +0330

    rendered the word meaning

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,32 +1,34 @@
-import React ,{useState} from "react";
+/** @format */
+
+import React, { useState } from "react";
 import axios from "axios";
-import "./Dictionary.css"
+import Resault from "./Resault";
+import "./Dictionary.css";
 
+export default function Dictionary() {
+	let [keyword, setKeyword] = useState("");
+	let [resault, setResault] = useState(null);
 
-export default function Dictionary(){
-  let[keyword , setKeyword]=useState("");
+	function handleResponse(response) {
+    setResault(response.data[0]);
+	}
 
+	function search(event) {
+		event.preventDefault();
+		// documentation : https://dictionaryapi.dev/
+		let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+		axios.get(apiUrl).then(handleResponse);
+	}
+	function handleChange(event) {
+		setKeyword(event.target.value);
+	}
 
-  function handleResponse(response){
-    console.log(response.data[0]);
-  }
-
-function search(event){
-	event.preventDefault();
-	// documentation : https://dictionaryapi.dev/
-	let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-	axios.get(apiUrl).then(handleResponse);
-}
-function handleChange(event){
-  setKeyword(event.target.value);
-}
-
-
-  return (
+	return (
 		<div className="Dictionary">
 			<form onSubmit={search}>
 				<input type="search" onChange={handleChange} />
 			</form>
+			<Resault resaults={resault}/>
 		</div>
 	);
 }
