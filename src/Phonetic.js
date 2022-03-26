f238e44182814416d04d39b9630736866d276ff0
noush-in <noushinheidari1990@gmail.com>
2022-03-26 12:05:04 +0430

    phonetic component added

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,12 +3,13 @@
 import React from "react";
 
 export default function Phonetic(props) {
-  console.log(props.Phonetic);
-	return(
-    <div className="Phonetic">
-      {props.Phonetic.text}
-      <br />
-      <a href={props.phonetic.audio} target="_blank">Listen</a>
-      </div>
-    );
+	return (
+		<div className="Phonetic">
+			{props.phonetic.text}
+			<br />
+			<a href={props.phonetic.audio} target="_blank">
+				Listen
+			</a>
+		</div>
+	);
 }
