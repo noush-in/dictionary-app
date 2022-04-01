10bea31989433c194cb0f0f70d82466ef702c9a9
noush-in <noushinheidari1990@gmail.com>
2022-04-01 13:19:01 +0430

    final touch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
+/** @format */
+
 import Dictionary from "./Dictionary";
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
+	return (
 		<div className="container">
 			<div className="App">
-				<header className="App-header text-center">Dictionary </header>
-				<main >
-					<Dictionary defaultKeyword="sunset"/>
+				<main>
+					<Dictionary defaultKeyword="sunset" />
 				</main>
-				<footer className="App-footer">Coded by Noushin Heidari</footer>
+				<footer className="App-footer">
+					Coded by{" "}
+					<a
+						href="https://www.linkedin.com/in/noushin-heidari-a04748121/"
+						target="_blank"
+						rel="noreferrer">
+						Noushin Heidari
+					</a>{" "}
+					,it's open-sourced on{" "}
+					<a
+						href="https://github.com/noush-in/dictionary-app"
+						target="_blank"
+						rel="noreferrer">
+						GitHub
+					</a>{" "}
+					, hosted on{" "}
+					<a
+						href="https://fanciful-toffee-798d3a.netlify.app/"
+						target="_blank"
+						rel="noreferrer">
+						netlify.
+					</a>
+				</footer>
 			</div>
 		</div>
 	);
